3a04b83bb961168ccfc06fa047f7c668188998d3
Diego Duarte <diemanuel58@gmail.com>
2022-04-27 22:54:35 -0400

    Sacar numeros del github por lenguaje, sin alias

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import requests
+import re
+from bs4 import BeautifulSoup
 from scraping import tiobescraper
 def main():
     list = tiobescraper.scrapeTiobe()
     print(list)
-
+    languages=list
+    for language in languages:
+        link="https://github.com/topics/"+language
+        gitpage=requests.get(link)
+        githubsoup = BeautifulSoup(gitpage.text, features="html.parser")
+        gitLenCant = githubsoup.find(class_="h3 color-fg-muted").text
+        
+        gitLenCant = re.search("\d+(,\d*)*",gitLenCant).group()
+        gitLenCant = language + " = " + gitLenCant
+        print (gitLenCant)
 
 main()
